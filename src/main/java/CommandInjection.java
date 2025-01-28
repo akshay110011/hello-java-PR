0994a4c88d48b715e0b4ebde92a598521d9fefc9
akshay110011 <45433553+akshay110011@users.noreply.github.com>
2025-01-27 19:09:40 -0500

    Update CommandInjection.java

--- src/main/java/CommandInjection.java
+++ src/main/java/CommandInjection.java
@@ -5,7 +5,7 @@ public class CommandInjection {
     public static Process runCmd(HttpServletRequest request) throws IOException {
         String filename = request.getParameter("filename");
         ProcessBuilder builder = new ProcessBuilder("cat", filename);
-        Process process = builder.start();
+        Process process = builder.start(); 
         return(process);
     }
 }
